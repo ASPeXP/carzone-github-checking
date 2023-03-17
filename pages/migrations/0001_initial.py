bbf5eeffca13785e3e165e6f38eab8a670514959
ASPeXP <prapaths@hotmail.com>
2023-03-18 06:52:13 +0700

    Added Features Cars admin and listing on home page

--- pages/migrations/0001_initial.py
+++ pages/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Teams',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('fist_name', models.CharField(max_length=255)),
                 ('last_name', models.CharField(max_length=255)),
                 ('designation', models.CharField(max_length=255)),
