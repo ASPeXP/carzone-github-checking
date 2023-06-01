a31a83774d2c452d3c990ae5c97de32731db8a43
ASPeXP <prapaths@hotmail.com>
2023-06-01 15:02:00 +0700

    Moving environ to top file

--- carzone/settings.py
+++ carzone/settings.py
@@ -15,7 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -102,10 +105,7 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 # Render PostgreSQL database (Live)
 import dj_database_url
-import environ
 
-env = environ.Env()
-environ.Env.read_env()
 
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
