aee8ef366e875c38a7413ec983e2ee1b89f5477d
ASPeXP <prapaths@hotmail.com>
2023-06-02 00:03:52 +0700

    move AWS_Access key, AWS secret access key to env variable

--- carzone/settings.py
+++ carzone/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'storages',
 
     # Providers
     'allauth.socialaccount.providers.facebook',
@@ -175,3 +176,13 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ipathsaid@gmail.com'
 EMAIL_HOST_PASSWORD = 'tfwgidmpdosbdwgw'
 EMAIL_USE_TLS = True
+
+# AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID="AKIAVERX2TF6ZHGPJV5S"
+AWS_SECRET_ACCESS_KEY="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"
+
+AWS_STORAGE_BUCKET_NAME = "carzone-bucket"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
